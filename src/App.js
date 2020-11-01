6d19727d9cbdb8d8da34f4bc6a15d1952251fa98
broccolism <mile3880@gmail.com>
2020-11-01 19:16:18 +0900

    moving

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import logo from './assets/broccoli.png';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import countdown from 'countdown';
 
-function App() {
-  return (
+function tick() {
+  const month = countdown(new Date(2020, 12, 1)).months.toString();
+  const day = countdown(new Date(2020, 12, 1)).days.toString();
+  // const hour = countdown(new Date(2020, 12, 1)).hours.toString();
+  // const minute = countdown(new Date(2020, 12, 1)).minutes.toString();
+  // const second = countdown(new Date(2020, 12, 1)).seconds.toString();
+
+  const element = (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Time left to be more delicious broccoli salad:
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          {month} month and {day} days
+        </div>
       </header>
     </div>
   );
+
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
+}
+
+function App() {
+  return setInterval(tick, 1000);
 }
 
 export default App;
