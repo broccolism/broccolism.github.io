ff860c9efc963ce3e6a2c8aa1443e19d6f1c963c
broccolism <mile3880@gmail.com>
2020-12-25 02:55:00 +0900

    commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from "./assets/broccoli.png";
+import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import countdown from "countdown";
+
+function tick() {
+  const month = countdown(new Date(2020, 12, 1)).months.toString();
+  const day = countdown(new Date(2020, 12, 1)).days.toString();
+  const element = (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Time left to be more delicious broccoli salad:</p>
+        <div>{day} days</div>
+      </header>
+    </div>
+  );
+
+  ReactDOM.render(element, document.getElementById("root"));
+}
+
+function App() {
+  return setInterval(tick, 1000);
+}
+
+export default App;
