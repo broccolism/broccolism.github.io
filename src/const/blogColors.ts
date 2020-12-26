e12ec388d00f372d36753df20a92041067bf21f2
broccolism <mile3880@gmail.com>
2020-12-26 23:22:32 +0900

    feat: colors

---
+++ src/const/blogColors.ts
@@ -0,0 +1,19 @@
+class BlogColors {
+  static Rose = ["#EA5368", "#F67D8E", "#FEA7B3", "#FFD8DD"];
+  static Salmon = ["#EFA19D", "#FDBCB8", "#FFD8D6", "#FFEEED"];
+  static Cream = ["#F2CFB1", "#FEE4CD", "#FFF0E3", "#FFF8F1"];
+  static Khaki = ["#C8C8AC", "#DFDFC0", "#F4F4D6", "#FDFDED"];
+  static Padua = ["#8CAE9C", "#A8D0BB", "#D5F2E3", "#F2FDF7"];
+  static LightGray = ["#F3F4F4", "#E3E5E4", "#C9CCCA", "#AEB1B0"];
+  static DarkGray = ["#111212", "#424544", "#646866", "#838986"];
+
+  static rose = "#EA5368";
+  static salmon = "#EFA19D";
+  static cream = "#F2CFB1";
+  static khaki = "#C8C8AC";
+  static padua = "#8CAE9C";
+  static lightGray = "#F3F4F4";
+  static darkGray = "#424544";
+}
+
+export default BlogColors;
