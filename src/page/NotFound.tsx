b77c5ad9768a5d92b62166f13dcd9f21b88bb602
broccolism <mile3880@gmail.com>
2020-12-28 23:22:03 +0900

    feating

--- src/page/NotFound.tsx
+++ src/page/NotFound.tsx
@@ -1,4 +1,9 @@
+import { createStyles } from "@material-ui/core";
 import React from "react";
+
+const styles = createStyles({});
+
+interface ComponentProps {}
 
 function NotFound() {
   return <div></div>;
