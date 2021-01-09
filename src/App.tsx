07eaa421cd3a75e166e733765f6bb19e75a02945
broccolism <mile3880@gmail.com>
2021-01-09 18:50:05 +0900

    feat: test for auto deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Moving Blog....</p>
+        <p>with AWS</p>
       </header>
     </div>
   );
