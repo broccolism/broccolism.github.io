942a37b2ba8a0522675712046d1adc9e8861f6d0
broccolism <mile3880@gmail.com>
2021-01-27 20:40:35 +0900

    feat: header

---
+++ src/consts/colors.ts
@@ -0,0 +1,22 @@
+class BlogColors {
+  static light = {
+    rose: "#EA5368",
+    salmon: "#EFA19D",
+    cream: "#F2CFB1",
+    khaki: "#C8C8AC",
+    padua: "#8CAE9C",
+    lightGray: "#C9CCCA",
+    darkGray: "#111212",
+  };
+  static dark = {
+    rose: "#FEA7B3",
+    salmon: "#FFD8D6",
+    cream: "#FEE4CD",
+    khaki: "#DFDFC0",
+    padua: "#D5F2E3",
+    lightGray: "#646866",
+    darkGray: "#111212",
+  };
+}
+
+export default BlogColors;
