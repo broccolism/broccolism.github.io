4748aafa437338ca961d45877bcce2e37300d659
broccolism <mile3880@gmail.com>
2021-11-10 20:40:22 +0900

    [MASTER] :wrencH: gatsby-meta-config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -24,66 +24,64 @@ module.exports = {
     },
   },
 
-  // // metadata for About Page
-  // about: {
-  //   timestamps: [
-  //     // =====       [Timestamp Sample and Structure]      =====
-  //     // ===== 🚫 Don't erase this sample =====
-  //     {
-  //       date: '',
-  //       activity: '',
-  //       links: {
-  //         github: '',
-  //         post: '',
-  //         googlePlay: '',
-  //         appStore: '',
-  //         demo: '',
-  //       },
-  //     },
-  //     // ========================================================
-  //     // ========================================================
-  //     {
-  //       date: '2021.11 ~',
-  //       activity: '개인 블로그 개발 및 운영',
-  //       links: {
-  //         post: '/gatsby-starter-zoomkoding-introduction',
-  //         github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-  //         demo: 'https://www.zoomkoding.com',
-  //       },
-  //     },
-  //   ],
+  // metadata for About Page
+  about: {
+    timestamps: [
+      // =====       [Timestamp Sample and Structure]      =====
+      // ===== 🚫 Don't erase this sample =====
+      {
+        date: '',
+        activity: '',
+        links: {
+          github: '',
+          post: '',
+          googlePlay: '',
+          appStore: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        date: '2021.11 ~',
+        activity: 'blog start!',
+        links: {
+          // post: '/gatsby-starter-zoomkoding-introduction',
+          // github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          // demo: 'https://www.zoomkoding.com',
+        },
+      },
+    ],
 
-  //   projects: [
-  //     // =====        [Project Sample and Structure]        =====
-  //     // ===== 🚫 Don't erase this sample =====
-  //     {
-  //       title: '',
-  //       description: '',
-  //       techStack: ['', ''],
-  //       thumbnailUrl: '',
-  //       links: {
-  //         post: '',
-  //         github: '',
-  //         googlePlay: '',
-  //         appStore: '',
-  //         demo: '',
-  //       },
-  //     },
-  //     // ========================================================
-  //     // ========================================================
-  //     {
-  //       title: '개발 블로그 테마 개발',
-  //       description:
-  //         '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-  //       techStack: ['gatsby', 'react'],
-  //       thumbnailUrl: 'blog.png',
-  //       links: {
-  //         post: '/gatsby-starter-zoomkoding-introduction',
-  //         github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-  //         demo: 'https://www.zoomkoding.com',
-  //       },
-  //     },
-  //   ],
-  // },
-  about: {},
+    projects: [
+      // =====        [Project Sample and Structure]        =====
+      // ===== 🚫 Don't erase this sample =====
+      {
+        title: '',
+        description: '',
+        techStack: ['', ''],
+        thumbnailUrl: '',
+        links: {
+          post: '',
+          github: '',
+          googlePlay: '',
+          appStore: '',
+          demo: '',
+        },
+      },
+      // ========================================================
+      // ========================================================
+      {
+        title: 'tmp',
+        description: '(tmp)',
+        techStack: ['gatsby', 'react'],
+        thumbnailUrl: 'blog.png',
+        links: {
+          // post: '/gatsby-starter-zoomkoding-introduction',
+          // github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          // demo: 'https://www.zoomkoding.com',
+        },
+      },
+    ],
+  },
 };
